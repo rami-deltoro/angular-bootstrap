133128c0d941557cd774bd4e65f71b9e319188d5
rami stefanidis <rami.stefanidis@gmail.com>
2019-04-01 21:08:46 -0400

    template updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tutorial-angular-cli-v6-styling-bootstrap';
+  title = 'Call Tracker';
 }
